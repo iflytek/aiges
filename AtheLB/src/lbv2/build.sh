7a53af5615a60233b90334a4f74b0ce3fddadbf7
scw00 <scw00@apache.org>
2019-10-17 11:17:29 +0800

    Adds build script for project building

--- AtheLB/src/lbv2/build.sh
+++ AtheLB/src/lbv2/build.sh
@@ -5,7 +5,7 @@ export PATH=${GOROOT}/bin:$PATH
 export GOPATH=`pwd`/../../
 
 go clean
-go build -v -o lbv2 #编译
-./lbv2 -v
+go build -v -o AtheLB #编译
+./AtheLB -v
 
 #./lbv2 -m 0 -c lbv2.toml  -s lbv2
