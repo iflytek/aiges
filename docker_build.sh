9610dbf37ecea960a0fcf21d88d3f48f33e6eff2
scw00 <scw00@apache.org>
2019-11-21 14:04:32 +0800

    aa

--- docker_build.sh
+++ docker_build.sh
@@ -3,3 +3,4 @@
 docker build -f Dockerfile.AtheProxy -t littlescw00/atheproxy:latest  .
 docker build -f Dockerfile.AtheGateway -t littlescw00/athegateway:latest  .
 docker build -f Dockerfile.AtheLB -t littlescw00/athelb:latest  .
+docker build -f Dockerfile.AtheLoader -t littlescw00/atheloader:latest  .
