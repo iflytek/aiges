34b414193ecb2da1ee2d06ed8f1fb7ed602c057f
unknown <kjsheng@iflytek.com>
2022-04-06 10:44:43 +0800

    fix build shell

--- src/github.com/xfyun/aiges/test/wrapper/build.sh
+++ src/github.com/xfyun/aiges/test/wrapper/build.sh
@@ -1,2 +1,2 @@
-gcc -fPIC -shared -Wno-attributes -g -O0 -rdynamic -o libwrapper.so wrapper.cpp
-gcc -fPIC -shared -Wno-attributes -std=c++0x -g -O0 -rdynamic -o libwrapper-catch.so wrapper_catch.cpp
+g++ -fPIC -shared -Wno-attributes -std=c++0x -g -O0 -rdynamic -o libwrapper.so wrapper.cpp
+g++ -fPIC -shared -Wno-attributes -std=c++0x -g -O0 -rdynamic -o libwrapper-catch.so wrapper_catch.cpp
