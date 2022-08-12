2b05a78a0d5a557cb1bdfedbcad3ac69e9288bbe
yqwu18 <yqwu18@iflytek.com>
2022-08-12 10:48:32 +0800

    delete demo directory

--- demo/mmocr/wrapper/configs/_base_/schedules/schedule_adadelta_5e.py
+++
@@ -1,6 +0,0 @@
-# optimizer
-optimizer = dict(type='Adadelta', lr=1.0)
-optimizer_config = dict(grad_clip=None)
-# learning policy
-lr_config = dict(policy='step', step=[])
-runner = dict(type='EpochBasedRunner', max_epochs=5)
--- demo/mmocr/wrapper/mmocr/models/common/losses/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .dice_loss import DiceLoss
-from .focal_loss import FocalLoss
-
-__all__ = ['DiceLoss', 'FocalLoss']
