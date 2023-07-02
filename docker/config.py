ff74e82d0cf0f72a106a1e4a770c898ae329818d
ybyang <ybyang7@iflytek.com>
2023-07-02 16:51:18 +0800

    fix:  base add libsqlite3-dev

--- docker/config.py
+++ docker/config.py
@@ -9,7 +9,8 @@ log.addHandler(ch)
 SUPPORTED_DISTRO_LIST = ["ubuntu1804"]
 SUPPORTED_PYVERSION_LIST = ["3.9.13", "conda-3.7", "conda-3.8", "conda-3.9"]
 SUPPORTED_GOLANG_LIST = ["1.17"]
-SUPPORTED_CUDA_LIST = ["10.1", "10.2", "11.2", "11.6"]
+#UPPORTED_CUDA_LIST = ["10.1", "10.2", "11.2", "11.6"]
+SUPPORTED_CUDA_LIST = ["11.2", "11.6"]
 
 # ECR_REPO = "public.ecr.aws/iflytek-open"
 ECR_REPO = "iflyopensource"
