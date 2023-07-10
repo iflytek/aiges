bbe84e91a5a46978a8ee3f5755666d864ecf3c98
ybyang <ybyang7@iflytek.com>
2023-07-10 15:19:43 +0800

    add 11.7 cuda

--- docker/config.py
+++ docker/config.py
@@ -10,7 +10,7 @@ SUPPORTED_DISTRO_LIST = ["ubuntu1804"]
 SUPPORTED_PYVERSION_LIST = ["3.9.13", "conda-3.7", "conda-3.8", "conda-3.9"]
 SUPPORTED_GOLANG_LIST = ["1.17"]
 #UPPORTED_CUDA_LIST = ["10.1", "10.2", "11.2", "11.6"]
-SUPPORTED_CUDA_LIST = ["11.2", "11.6"]
+SUPPORTED_CUDA_LIST = ["11.2", "11.6","11.7"]
 
 # ECR_REPO = "public.ecr.aws/iflytek-open"
 ECR_REPO = "iflyopensource"
