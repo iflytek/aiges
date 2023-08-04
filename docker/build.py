698a55663f708a117e6986f7d9fd756b55a2154f
ybyang <ybyang7@iflytek.com>
2023-08-04 14:22:22 +0800

    fix : add bussines

--- docker/build.py
+++ docker/build.py
@@ -186,7 +186,7 @@ class ManagerGenerate(Manager):
         base_images_list = [
             release_line_format.format(registry=self.get_regsitry(), tag=str(tag), python=tag.python, cuda=tag.cuda,
                                        distro=tag.distro, repo='cuda-go-python-base', git_tag="")
-            if not tag.is_bussiness else tag for tag in self.matrix]
+            for tag in self.matrix if not tag.is_bussiness]
         log.info(base_images_list)
         aiges_images_list = [
             release_line_format.format(registry=self.get_regsitry(), tag=str(tag), python=tag.python, cuda=tag.cuda,
